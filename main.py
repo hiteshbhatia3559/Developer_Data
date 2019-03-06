84a35c445bd45907d38bbb184127a15fc590468a
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-03-06 14:34:19 +0530

    PEP-8 compliance

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import play_scraper
-from play_scraper import lists
 import time
 
-f = open('result.txt','w')
+f = open('result.txt', 'w')
 
 scraper = play_scraper.collection(
     collection='TRENDING',
-    results = 10,
-    page = 0
+    results=10,
+    page=0
 
 )
 
 list_of_ids = []
-list_of_emails = []
+list_of_details = []
 for item in scraper:
     list_of_ids.append(item['app_id'])
 
 # print(list_of_ids)
 
 for id in list_of_ids:
-    a = play_scraper.details(id)['developer_email']
-    print(a)
-    f.write(str(a)+',\n')
-    list_of_emails.append(a)
+    a = play_scraper.details(id)
+    b = {a['app_id']: [a['title'], a['developer_id'], a['installs'], a['developer_url'], a['developer_email']]}
+    list_of_details.append(b)
+    f.write(str(b) + '\n')
 
+print(list_of_details)
