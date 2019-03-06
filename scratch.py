ed23acce3795069c3629aa7f619e5166b2a6059f
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-03-06 14:30:51 +0530

    Milestone 1 Revision - Added more parameters and modified write type to a dictionary.

---
+++ scratch.py
@@ -0,0 +1,26 @@
+import play_scraper
+import time
+
+f = open('result.txt','w')
+
+scraper = play_scraper.collection(
+    collection='TRENDING',
+    results = 10,
+    page = 0
+
+)
+
+list_of_ids = []
+list_of_emails = []
+for item in scraper:
+    list_of_ids.append(item['app_id'])
+
+# print(list_of_ids)
+
+for id in list_of_ids:
+    a = play_scraper.details(id)
+    b = {a['app_id']:[a['title'],a['developer_id'],a['installs'],a['developer_url'],a['developer_email']]}
+    list_of_emails.append(b)
+    f.write(str(b)+'\n')
+
+print(list_of_emails)
